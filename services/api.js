f5cae844363972282e060030f0ad881ba03eb4ec
Jeverson Santos <jeverson.apsantos@gmail.com>
2024-11-11 19:38:28 -0300

    conclusao do desafio

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bmdjYnBkbXRpYWlyb2dhZXZhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTM1NDQ5OSwiZXhwIjoyMDQ2OTMwNDk5fQ.Lv2sfoyxHnhluSxf_v2fRzHGfn8TH-F9IfWHAv3xH5g",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6bmdjYnBkbXRpYWlyb2dhZXZhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTM1NDQ5OSwiZXhwIjoyMDQ2OTMwNDk5fQ.Lv2sfoyxHnhluSxf_v2fRzHGfn8TH-F9IfWHAv3xH5g"
     }
 })
